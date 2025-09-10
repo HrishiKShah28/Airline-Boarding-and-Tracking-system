56ab4cf40e4b634ed0c25de09c359707ad8657f0
Yash Darji <work.yashmdarji@gmail.com>
2025-09-11 00:58:21 +0530

    Create businessClass.cpp

---
+++ businessClass.cpp
@@ -0,0 +1,11 @@
+class BusinessPassenger : public Passenger {
+public:
+    BusinessPassenger(string n, string p, string seat)
+        : Passenger(move(n), move(p), move(seat), true) {}
+
+    void boardFlight() override {
+        updateStatus(Status::BoardedBusiness);
+        cout << "Business Passenger " << getName()
+             << " (Seat " << getSeatNo() << ") boarded.\n";
+    }
+};
