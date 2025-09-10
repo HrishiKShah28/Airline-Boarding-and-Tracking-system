d7439cb03ff1d5820d4d2428d80f3d2580008c17
Yash Darji <work.yashmdarji@gmail.com>
2025-09-11 00:57:07 +0530

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -60,14 +60,4 @@ public:
     }
 };
 
-class EconomyPassenger : public Passenger {
-public:
-    EconomyPassenger(string n, string p, string seat)
-        : Passenger(move(n), move(p), move(seat), false) {}
 
-    void boardFlight() override {
-        updateStatus(Status::BoardedEconomy);
-        cout << "Economy Passenger " << getName()
-             << " (Seat " << getSeatNo() << ") boarded.\n";
-    }
-};
