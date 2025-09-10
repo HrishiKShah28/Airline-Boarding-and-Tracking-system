a6f74632e7c0696119b4166fc3cfea55d9c0a38d
HrishiKShah28 <hrishi28june@gmail.com>
2025-09-11 01:07:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
-#include "passenger.h"
-#include "flightclass.cpp" // better to use flight.h later
+#include <iostream>
+#include <memory>
+using namespace std;
+
+#include "Passenger.h"
+#include "BusinessClass.h"
+#include "EconomyClass.h"
+#include "FlightClass.h"
 
 int main() {
     Flight f("AI202", "Delhi", "Mumbai", "G12");
 
-    f.addPassenger(std::make_unique<BusinessPassenger>("Raj", "P123", "1A"));
-    f.addPassenger(std::make_unique<EconomyPassenger>("Anita", "P456", "12B"));
-    f.addPassenger(std::make_unique<EconomyPassenger>("Karan", "P789", "15C"));
+    f.addPassenger(make_unique<BusinessPassenger>("Raj", "P123", "1A"));
+    f.addPassenger(make_unique<EconomyPassenger>("Anita", "P456", "12B"));
+    f.addPassenger(make_unique<EconomyPassenger>("Karan", "P789", "15C"));
 
     f.startBoarding();
     f.showPassengerStatus();
