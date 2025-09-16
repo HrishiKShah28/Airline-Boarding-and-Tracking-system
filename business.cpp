f527ff52c9fe5fdcb2e88df2236fbc9567ed3008
Yash Darji <work.yashmdarji@gmail.com>
2025-09-16 21:58:55 +0530

    Added Default Arguments, Dynamic Memory Allocation and Friend Functions

--- business.cpp
+++ business.cpp
@@ -2,84 +2,83 @@
 using namespace std;
 
 class BusinessPassenger : public Passenger {
-    bool loungeAccess;
-    bool priorityBoarding;
-    int baggageWeight;
-    double baseFare;
-    string seatNumber;
-    bool hasBoarded;
+    bool lounge;
+    bool priority;
+    int bags;
+    double fare;
+    string seat;
+    bool boarded;
 
 public:
     BusinessPassenger() : Passenger() {
-        loungeAccess = true;
-        priorityBoarding = true;
-        baggageWeight = 0;
-        baseFare = 1500.0;
-        hasBoarded = false;
-        seatNumber = "Not Assigned";
+        lounge = true;
+        priority = true;
+        bags = 0;
+        fare = 1500.0;
+        boarded = false;
+        seat = "Not Assigned";
         cout << "Business Passenger created (Default Constructor)" << endl;
     }
 
-    BusinessPassenger(string id, string n, int a, string passport, 
-                      int bagWeight = 30) 
+    BusinessPassenger(string id, string n, int a, string passport, int b = 30) 
         : Passenger(id, n, a, passport) {
-        loungeAccess = true;
-        priorityBoarding = true;
-        baggageWeight = bagWeight;
-        baseFare = 1500.0;
-        hasBoarded = false;
-        seatNumber = "Not Assigned";
+        lounge = true;
+        priority = true;
+        bags = b;
+        fare = 1500.0;
+        boarded = false;
+        seat = "Not Assigned";
         cout << "Business Passenger created for " << name << endl;
     }
 
-    void displayInfo() const {
+    void show() const {
         cout << "\nBUSINESS CLASS" << endl;
         Passenger::displayInfo();
-        cout << "Lounge Access: " << (loungeAccess ? "Yes" : "No") << endl;
-        cout << "Priority Boarding: " << (priorityBoarding ? "Yes" : "No") << endl;
-        cout << "Baggage Weight: " << baggageWeight << " kg" << endl;
-        cout << "Seat Number: " << seatNumber << endl;
-        cout << "Total Fare: $" << calculateFare() << endl;
+        cout << "Lounge Access: " << (lounge ? "Yes" : "No") << endl;
+        cout << "Priority Boarding: " << (priority ? "Yes" : "No") << endl;
+        cout << "Baggage: " << bags << " kg" << endl;
+        cout << "Seat: " << seat << endl;
+        cout << "Total Fare: $" << totalFare() << endl;
     }
 
-    double calculateFare() {
-        double totalFare = baseFare;
-        if (baggageWeight > 40) totalFare += (baggageWeight - 40) * 25;
-        return totalFare;
+    double totalFare() {
+        double t = fare;
+        if (bags > 40) t += (bags - 40) * 25;
+        return t;
     }
 
-    void boardPassenger() {
-        hasBoarded = true;
+    void board() {
+        boarded = true;
         cout << "PRIORITY BOARDING" << endl;
-        cout << "Business class passenger " << name << " has priority boarded." << endl;
+        cout << "Business passenger " << name << " boarded." << endl;
     }
 
-    void accessLounge() {
-        if (loungeAccess) cout << name << " is accessing the Business Lounge." << endl;
+    void loungeUse() {
+        if (lounge) cout << name << " is in Business Lounge." << endl;
     }
 
-    void assignSeat(string seat) {
-        seatNumber = seat;
-        cout << "Premium seat " << seat << " assigned to " << name << endl;
+    void setSeat(string s) {
+        seat = s;
+        cout << "Premium seat " << s << " assigned to " << name << endl;
     }
 
-    void allocateExtraBaggage(int extraKg) {
-        int* extra = new int(extraKg);
-        baggageWeight += *extra;
-        cout << "Extra " << *extra << " kg baggage allocated to " << name << endl;
-        delete extra;
+    void addBags(int extra) {
+        int* x = new int(extra);
+        bags += *x;
+        cout << "Extra " << *x << " kg baggage added for " << name << endl;
+        delete x;
     }
 
     ~BusinessPassenger() {
         cout << "Business Passenger " << name << " destroyed" << endl;
     }
 
-    friend void compareBusinessFare(BusinessPassenger& p1, BusinessPassenger& p2) {
-        if (p1.calculateFare() > p2.calculateFare())
-            cout << p1.name << " has a higher fare." << endl;
-        else if (p1.calculateFare() < p2.calculateFare())
-            cout << p2.name << " has a higher fare." << endl;
+    friend void compareFare(BusinessPassenger& p1, BusinessPassenger& p2) {
+        if (p1.totalFare() > p2.totalFare())
+            cout << p1.name << " has higher fare." << endl;
+        else if (p1.totalFare() < p2.totalFare())
+            cout << p2.name << " has higher fare." << endl;
         else
-            cout << "Both business passengers have equal fare." << endl;
+            cout << "Both have equal fare." << endl;
     }
 };
